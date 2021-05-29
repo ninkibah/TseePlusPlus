7583c44f6974e30c78da3effa6f0d44593bbbfef
Jonathan <jonathan.oconnor@theclustercompany.de>
2021-05-29 11:15:27 +0100

    anfängliches Commit

---
+++ Hauptmodule.cpp
@@ -0,0 +1,10 @@
+#include "TseePlusPlus.hpp"
+#include <eingabe_ausgabe_fluss>
+
+ganzzahl haupt() {
+    standard::konsole_ausgabe_fluss << "Hallo Welt!" << std::zeilen_ende;
+    rückgabe 0;
+}
+
+
+//fahrzeug ergebnisse = nummern | standard::ansichten::filter([](ganzzahl n){ rückgabe n % 2 == 0; }) | standard::ansichten::umwandeln([](ganzzahl n){ rückgabe n * 2; });
---
+++ TseePlusPlus.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+
+#define Typname typename
+#define Schablone template
+#define haupt main
+#define verwert using
+#define Namensraum namespace
+#define rückgabe return
+#define konsole_ausgabe_fluss cout
+#define zeilen_ende endl
+
+#define eingabe_ausgabe_fluss iostream
+
+verwert leere = void;
+verwert fahrzeug = char;
+verwert ganzzahl = int;
+Namensraum standard = std;
