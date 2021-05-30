8e241cfe95a4ce3eae19a27ac5494d84c4c8e8be
Simon Renger <simonrenger@users.noreply.github.com>
2021-05-30 12:36:57 +0200

    applied grammar changes

--- Hauptmodule.cpp
+++ Hauptmodule.cpp
@@ -2,19 +2,19 @@
 #include <eingabe_ausgabe_fluss>
 #include <ansichten>
 
-leere beschissen() {
+Leere beschissen() {
     //fahrzeug ergebnisse = nummern | standard::ansichten::filter([](ganzzahl n){ rückgabe n % 2 == 0; }) | standard::ansichten::umwandeln([](ganzzahl n){ rückgabe n * 2; });
 }
 
-leere VektorenBeispiel() {
-    standard::vektor<fahrzeug> Wort{'H', 'a', 'l', 'l', 'o'};
-    für(auto i = 0; i < Wort.grosse(); ++i) {
-        standard::konsole_ausgabe_fluss << Wort[i];
+Leere VektorenBeispiel() {
+    Standard::Vektor<Zeichen> Wort{'H', 'a', 'l', 'l', 'o'};
+    für(auto i = 0; i < Wort.Größe(); ++i) {
+        Standard::Konsoleausgabe_fluss << Wort[i];
     }
 
 }
-ganzzahl haupt() {
-    standard::konsole_ausgabe_fluss << "Hallo Welt!" << std::zeilen_ende;
+Ganzzahl Haupt() {
+    Standard::Konsoleausgabe_fluss << "Hallo Welt!" << std::Zeilenende;
     rückgabe 0;
 }
 
